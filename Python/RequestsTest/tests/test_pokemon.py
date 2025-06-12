991a18905f268a808dc7ddbadd488ffcaae33059
Kirill Belyakov <127777833+belyakovko@users.noreply.github.com>
2025-06-12 21:55:26 +0500

    Add files via upload

--- Python/RequestsTest/tests/test_pokemon.py
+++ Python/RequestsTest/tests/test_pokemon.py
@@ -1,12 +1,15 @@
 import requests
 
+
 URL='https://api.pokemonbattle.ru/v2'
-TRAINER_ID='ID тренера'
+TRAINER_ID='ID_тренера'
+TRAINER_NAME='Имя_тренера'
 
 def test_status():
     response=requests.get(url=f'{URL}/trainers')
     assert response.status_code==200
 
-def test_trainers_id():
+def test_trainer_name():
     response=requests.get(url=f'{URL}/trainers', params={'trainer_id': TRAINER_ID})
-    assert response.json()["data"][0]["id"]==TRAINER_ID
+    assert response.json()["data"][0]["trainer_name"]==TRAINER_NAME
+
